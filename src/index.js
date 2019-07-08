287ab0cc72a51fc60efc84b1b5e685fb61716033
Behorsed <paulina.e.kowalik@wp.pl>
2019-07-08 22:33:19 +0200

    small refactoring; state moved to Test component to better handle componentDidUpdate

--- src/index.js
+++ src/index.js
@@ -8,10 +8,72 @@ class App extends Component {
         super(props);
         this.state = {
             startPage: true,
-            source: "/circle.gif",
-            timeList: [],
-            timeAppeared: Date.now(),
         };
+    }
+    handleMenuClick() {
+        this.setState({
+            startPage: true,
+        });
+    }
+    handleStartClick() {
+        this.setState({
+            startPage: false,
+        });
+    }
+
+    render() {
+        return(
+            this.state.startPage ? <div><
+                Title
+                onStartClick = {() => this.handleStartClick()}/></div> :
+                <div><Test
+                onMenuClick={() => this.handleMenuClick()}
+                /> </div>
+        );
+    }
+}
+class Title extends Component {
+
+    render() {
+        return (
+            <div id="title">
+                <h1>Reaction</h1>
+                <p>Rules: click on the circle as soon as it
+                    appears on the screen. The application will measure the time you needed to react.</p>
+                <button className="btn" onClick={() => this.props.onStartClick()}>Start</button>
+            </div>
+        );
+    }
+}
+class Buttons extends Component {
+    render() {
+        return (
+            <button className="btn" onClick={() => this.props.onMenuClick()}>Menu</button>
+        )
+    }
+}
+class Circle extends Component {
+
+    render() {
+        return (
+            <img alt = '' src={this.props.source} onClick={() => this.props.onCircleClick()}/>
+        )
+    }
+}
+class Test extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            timeListSimple: [],
+            timeAppeared: Date.now(),
+            source: '/circle.gif'
+        };
+    }
+    handleCircle() {
+        this.setState({
+            source: '',
+        })
+
     }
     componentDidUpdate(prevProps, prevState) {
         if (prevState.source === "/circle.gif" && this.state.source === '') { // if the circle disappeared
@@ -25,71 +87,15 @@ class App extends Component {
                     .bind(this),
                 time
             );
-            this.setState({ timeList: [...this.state.timeList, timeClicked - prevState.timeAppeared] });
+            this.setState({ timeListSimple: [...this.state.timeListSimple, timeClicked - prevState.timeAppeared] });
         }
-    }
-    handleClick() {
-        const startPage = this.state.startPage;
-        this.setState({
-            startPage: !startPage,
-        });
-    }
-    handleCircle() {
-        this.setState({
-            source: '',
-        })
-
     }
     render() {
         const source = this.state.source;
-        const timeList = this.state.timeList;
-        return(
-            this.state.startPage ? <div><
-                Title
-                onClick = {() => this.handleClick()}/><Table timeList = {timeList}/></div> :
-                <div><Test
-                onClick={() => this.handleClick()} onCircleClick={() => this.handleCircle()}
-                source = { source }
-                /> <Table timeList = {timeList}/> </div>
-        );
-    }
-}
-class Title extends Component {
-
-    render() {
-        return (
-            <div id="title">
-                <h1>Reaction</h1>
-                <p>Rules: click on the circle as soon as it
-                    appears on the screen. The application will measure the time you needed to react.</p>
-                <button className="btn" onClick={() => this.props.onClick()}>Start</button>
-            </div>
-        );
-    }
-}
-class Buttons extends Component {
-    render() {
-        return (
-            <button className="btn" onClick={() => this.props.onClick()}>Menu</button>
-        )
-    }
-}
-class Circle extends Component {
-
-    render() {
-        return (
-            <img alt = '' src={this.props.source} onClick={() => this.props.onCircleClick()}/>
-        )
-    }
-}
-class Test extends Component {
-
-    render() {
-        const source = this.props.source;
         return (
             <div>
-                <Buttons onClick={() => this.props.onClick()}/>
-                <Circle onCircleClick={() => this.props.onCircleClick()} source = {source}/>
+                <Buttons onMenuClick={() => this.props.onMenuClick()}/>
+                <Circle onCircleClick={() => this.handleCircle()} source = {source}/>
             </div>
         );
     }
