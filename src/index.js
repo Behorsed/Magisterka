e301b5ac569ed84411b311deefd628a0d0149594
Behorsed <paulina.e.kowalik@wp.pl>
2019-07-18 22:38:05 +0200

    standard deviation calculation

--- src/index.js
+++ src/index.js
@@ -61,19 +61,33 @@ class Circle extends Component {
     }
 }
 class Stats extends Component { // reusable component to
+
+    average(data){
+        const sum = data.reduce(function(sum, value){
+            return sum + value;
+        }, 0);
+
+        const avg = sum / data.length;
+        return avg;
+    }
     render() {
         const timeList = this.props.timeList;
-        if (timeList.length == 0) {
+        if (timeList.length === 0) {
             return (null);
         }
-        let sum = 0;
-        for( let i = 0; i < timeList.length; i++ ) {
-            sum += timeList[i];
-        }
-        const avg = Math.round(sum/timeList.length);
 
+        const avg = Math.round(this.average(timeList));
+        let stdDevElements = timeList.map(function(value){
+            let diff = value - avg; //(x-x_mean)
+            return diff * diff; // (x-x_mean)^2
+        });
+        const avgElements = this.average(stdDevElements); ////  (x-x_mean)^2 / N
+        const stdDev = Math.round(Math.sqrt(avgElements));
         return (
-            <p> Average: {avg} ms</p>
+            <div>
+             <p> Average: {avg} ms</p>
+             <p> Standard deviation: {stdDev} ms </p>
+            </div>
         )
     }
 }
